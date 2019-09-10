a7994c6546d610f413edd643f6db40d56b11df72
Thomas Johander <tom.johander@dealerteamwork.com>
2019-09-10 07:19:49 -0500

    Scanning long string

--- src/info/tjohander/Main.java
+++ src/info/tjohander/Main.java
@@ -1,8 +1,29 @@
 package info.tjohander;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
+
+    String fullText = "";
+
+    try {
+      FileReader reader = new FileReader(new File("/Users/lopan/sample-text.txt"));
+      Scanner scanner = new Scanner(reader);
+      while (scanner.hasNextLine()) {
+        fullText = fullText + scanner.nextLine();
+      }
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("Something went wrong");
+      e.printStackTrace();
+    }
+
+    System.out.println(fullText);
 
   }
 
