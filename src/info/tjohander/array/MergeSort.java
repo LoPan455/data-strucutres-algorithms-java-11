95eb740186b5ba83c6a6ce5d2955b33dc23c0120
Thomas Johander <tom.johander@dealerteamwork.com>
2019-10-10 20:25:34 -0500

    Done with this resource.  it's terrible

--- src/info/tjohander/array/MergeSort.java
+++ src/info/tjohander/array/MergeSort.java
@@ -3,12 +3,56 @@ package info.tjohander.array;
 import static info.tjohander.util.Util.*;
 
 import info.tjohander.util.Util;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
 
+
+/**
+ * All this one does is merge two arrays and then call Arrays.sort() on the resulting merged array.
+ */
 public class MergeSort {
 
   public static void main(String[] args) {
-    int[] array = Util.randomIntNumberGenerator(10, 50);
-  }
 
+    // int[] array1 = Util.randomIntNumberGenerator(8, 50);
+    int[] array1 = {22, 26, 29, 35, 36, 38, 38, 41};
+    Arrays.sort(array1);
+    System.out.println("Array 1: " + Arrays.toString(array1));
+    // int[] array2 = Util.randomIntNumberGenerator(5, 50);
+    int[] array2 ={3, 17, 23, 31, 46};
+    Arrays.sort(array2);
+    System.out.println("Array 2: " + Arrays.toString(array2));
+    int[] array3 = new int[array1.length + array2.length];
+
+    int i = 0, j = 0, k = 0;
+
+    Instant start = Instant.now();
+
+    while (i < array1.length && j < array2.length) {
+
+      // this part of the code isn't even necessary.  This exercise is just dumb.
+//      if (array1[i] < array2[i]) {
+//        array3[k++] = array1[i++];
+//      } else {
+//        array3[k++] = array2[j++];
+//      }
+      while (i < array1.length) {
+        array3[k++] = array1[i++];
+      }
+      while (j < array2.length) {
+        array3[k++] = array2[j++];
+      }
+    }
+
+
+    Arrays.sort(array3);
+    Instant end = Instant.now();
+    System.out.println("Array 3: " + Arrays.toString(array3));
+    System.out.println("Duration: " + Duration.between(start, end).toNanos());
+    int[] sample = new int[]{3, 17, 22, 23, 26, 29, 31, 35, 36, 38, 38, 41, 46};
+    if(Arrays.equals(array3, sample) ) {
+      System.out.println("The arrays are the same");
+    }
+  }
 }
--- src/info/tjohander/array/MergeSort2.java
+++ src/info/tjohander/array/MergeSort2.java
@@ -1,5 +1,32 @@
 package info.tjohander.array;
 
+import info.tjohander.util.Util;
+import java.util.Arrays;
+
+/**
+ * This is the actual merge sort algorithm
+ */
 public class MergeSort2 {
 
+  public static void main(String[] args) {
+    int[] array1 = Util.randomIntNumberGenerator(18, 50);
+    System.out.println(Arrays.toString(array1));
+    sort(array1, array1[0], array1.length - 1);
+
+  }
+
+  public static void sort(int[] array, int leftIndex, int rightIndex) {
+    System.out.println("Entering sort()...Value of 'array' is: " + Arrays.toString(array));
+
+    // This will tell us if the array has been broken down into it's smallest possible element
+    if (leftIndex < rightIndex) {
+
+      // This will get us the middle index of the array
+      int middle = (leftIndex + rightIndex) / 2;
+
+      // now we go into the recursion by first calling this same method on itself using the left array
+      sort(array, leftIndex, middle);
+      sort(array, middle + 1, rightIndex);
+    }
+  }
 }
